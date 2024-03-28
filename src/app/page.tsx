8b85228e2baab3d5b20c5cea77d82bd7d0cbb48d
Mazedul Islam <mazidmailbox@gmail.com>
2024-03-28 18:06:08 +0600

    feat: show toast message

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ import {
 
 export default function Home() {
   return (
-    <main className="flex  flex-col items-center justify-center p-24">
+    <main className="flex flex-col items-center justify-center p-24">
       <h1 className="text-4xl mb-24">Contact Me</h1>
       <Card>
         <CardHeader>
--- src/components/forms/ContactForm.tsx
+++ src/components/forms/ContactForm.tsx
@@ -1,13 +1,27 @@
+"use client";
 import React from "react";
 import { Label } from "../ui/label";
 import { Input } from "../ui/input";
 import { Textarea } from "../ui/textarea";
 import { sendEmail } from "@/actions/sendEmail";
 import SubmitButton from "./SubmitButton";
+import { useToast } from "../ui/use-toast";
 
 function ContactForm() {
+  const { toast } = useToast();
+
+  const handleSubmit = async (formData: FormData) => {
+    const { error, message } = await sendEmail(formData);
+
+    if (error) {
+      toast({ title: "Error", description: error, variant: "destructive" });
+    } else {
+      toast({ title: "Success", description: message, variant: "default" });
+    }
+  };
+
   return (
-    <form action={sendEmail} className="flex flex-col space-y-4">
+    <form action={handleSubmit} className="flex flex-col space-y-4">
       <div className="flex flex-col space-y-1.5">
         <Label htmlFor="name">Name</Label>
         <Input id="name" name="name" type="text" placeholder="John Doe" />
