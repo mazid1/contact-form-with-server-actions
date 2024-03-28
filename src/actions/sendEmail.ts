b3f4b6250f7d5b0fd49ff26a2a1cdc69837095eb
Mazedul Islam <mazidmailbox@gmail.com>
2024-03-28 16:26:59 +0600

    feat: reply-to button will target the sender email

--- src/actions/sendEmail.ts
+++ src/actions/sendEmail.ts
@@ -1,16 +1,17 @@
 "use server";
 import { createTransport } from "nodemailer";
 
-const SENDER_EMAIL = process.env.SENDER_EMAIL;
-const SENDER_PASSWORD = process.env.SENDER_PASSWORD;
+const RECEIVING_EMAIL = process.env.RECEIVING_EMAIL as string;
+const SENDING_EMAIL = process.env.SENDING_EMAIL as string;
+const SENDING_EMAIL_PASSWORD = process.env.SENDING_EMAIL_PASSWORD as string;
 
 const transporter = createTransport({
   host: "smtp.gmail.com",
   port: 465,
   secure: true,
   auth: {
-    user: SENDER_EMAIL,
-    pass: SENDER_PASSWORD,
+    user: SENDING_EMAIL,
+    pass: SENDING_EMAIL_PASSWORD,
   },
 });
 
@@ -20,19 +21,19 @@ export async function sendEmail(formData: FormData) {
   const message = formData.get("message") as string;
 
   const mailOptions = {
-    from: SENDER_EMAIL,
-    to: SENDER_EMAIL,
-    subject: `[Contact Me] ${name} <${email}>`,
-    text: `${message} | Reply to: ${email}`,
+    from: { name: "Contact Me", address: SENDING_EMAIL },
+    to: RECEIVING_EMAIL,
+    replyTo: email,
+    subject: `${name} <${email}>`,
+    text: message,
     html: `
       <div>${message.replace(/\n/g, "<br>")}</div>
-      <p>Reply to: ${email}</p>
     `,
   };
 
   try {
     const sentMessageInfo = await transporter.sendMail(mailOptions);
-    console.log("Message sent: %s", sentMessageInfo);
+    console.log("Message sent: %s", JSON.stringify(sentMessageInfo, null, 2));
     return { error: null, message: "Thank you for the message!" };
   } catch (err) {
     console.error(err);
